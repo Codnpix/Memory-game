043d3c57022363ca2bb1804393864620d0788edc
Codnpix <pierrejarriges@gmail.com>
2018-11-17 21:40:58 +0100

    ajout du tableau des joueurs et meilleurs scores au demarrage

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ if (isset($_POST['player'])) {
     fclose($score_file);
     if ($best_score == 0) {$best_score = "---";}
     $_SESSION['best_score'] = $best_score;
+    $players_list_file = fopen('players.txt', 'a+');
+    fputs($players_list_file, $player_name .";");
+    fclose($players_list_file);
+    
     ?>
 <!DOCTYPE html>
 <html>
@@ -47,6 +51,21 @@ else {
                     <label>Player : </label><br>
                     <input type="text" name="player" id="inputPlayer" autofocus> 
                     <input id='choosePlayerButton' value="OK" type="submit">
+                    <div id="playersList">
+                        <span class="listTitle">Joueurs : </span><br>
+                    <?php
+                    $players_list_file = fopen('players.txt', 'r');
+                    fseek($players_list_file, 0);
+                    $players_list = explode(";", fgets($players_list_file));
+                    for($i = 0 ; $i < (count($players_list) -1) ; $i ++) {
+                        $player = $players_list[$i];
+                        $player_score_file = fopen($player .'-score.txt', 'r');
+                        $player_score = fgets($player_score_file);
+                        fclose($player_score_file);
+                        echo '<b>' .$player .'</b> : Meilleur score &#62; ' .$player_score .' coups.<br>';
+                    }
+                    ?>
+                    </div>
                 </form>
             </section>
         </section>
