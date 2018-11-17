c195cbd8b48b4602ad3ea01c0b9e5bdd90422c41
Codnpix <pierrejarriges@gmail.com>
2018-11-18 00:43:33 +0100

    ajout de commentaires dans score.php et style.css

--- score.php
+++ score.php
@@ -1,15 +1,19 @@
 <?php
 session_start();
 if (isset($_GET['score'])) {
+    /*handle the current score value sent by the JS program*/
     $new_score = intval($_GET['score']);
     $file_path = $_SESSION['player'] .'-score.txt';
+    /*in case of first score, set bestscore at 1000 to make sure enter in the condition $new_score < $current_score.*/
     if ($_SESSION['best_score'] == "---") {$_SESSION['best_score'] = 1000;}
     $current_score = $_SESSION['best_score'];
+    /*update the best score in the players's score file*/
     if ($new_score < $current_score) {
         $score_file = fopen($file_path, 'w');
         fputs($score_file, $new_score);
         fclose($score_file);
     }
+    /*redirect to index*/
     header('Location: index.php');
     exit;
 }
